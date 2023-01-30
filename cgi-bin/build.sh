b99b0955fd49ac91ae8342e5ccb413d38c24adc8
youminds <99875574+youminds@users.noreply.github.com>
2023-01-30 21:51:04 +0000

    HTML

--- cgi-bin/build.sh
+++ cgi-bin/build.sh
@@ -5,6 +5,13 @@ sudo chmod 777 ../conf
 
 sudo chmod 666 ../conf/*.cfg
 
+# Copy HTML Files
+sudo cp ../html/*.html /var/www/html/
+sudo cp *.html /usr/lib/cgi-bin/
+sudo cp *.inc /usr/lib/cgi-bin/
+
+
+
 # Compile CGI
 sudo gcc template.cpp -lstdc++ -o "/usr/lib/cgi-bin/template.cgi"
 
