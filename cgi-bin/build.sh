4b7f59646beac691be2dec2982940ef06a31ff9a
youminds <99875574+youminds@users.noreply.github.com>
2023-02-03 11:44:04 +0000

    Final

--- cgi-bin/build.sh
+++ cgi-bin/build.sh
@@ -8,9 +8,11 @@ sudo chmod 777 ../conf
 sudo chmod 666 ../conf/*.cfg
 
 echo "Copy HTML Files"
-sudo cp ../html/*.html /var/www/html/
-sudo cp ../html/*.css /var/www/html/
-sudo cp ../html/*.png /var/www/html/
+sudo mkdir /var/www/html/icons/
+sudo cp -r ../html/* /var/www/html/
+
+
+
 
 sudo cp *.html /usr/lib/cgi-bin/
 sudo cp *.inc /usr/lib/cgi-bin/
